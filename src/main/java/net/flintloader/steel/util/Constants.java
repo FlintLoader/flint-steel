1c49f78b30879ca54f5c079f13d8c6e11ee039db
HypherionMC <hypherionmc@gmail.com>
2023-12-19 21:25:47 +0200

    Update Dev Launch Injector Version

--- src/main/java/net/flintloader/steel/util/Constants.java
+++ src/main/java/net/flintloader/steel/util/Constants.java
@@ -95,7 +95,7 @@ public class Constants {
 		 */
 		public static final class Versions {
 			public static final String MIXIN_COMPILE_EXTENSIONS = "0.5.0";
-			public static final String DEV_LAUNCH_INJECTOR = "0.1.1";
+			public static final String DEV_LAUNCH_INJECTOR = "1.0.0";
 			public static final String TERMINAL_CONSOLE_APPENDER = "1.2.0";
 			public static final String JETBRAINS_ANNOTATIONS = "23.0.0";
 			public static final String NATIVE_SUPPORT_VERSION = "1.0.1";
